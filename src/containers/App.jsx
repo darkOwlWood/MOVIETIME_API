1b85fd96dc3354de91100bbabe6db08fbf494a5c
Daniel Reyes <danielckings@gmail.com>
2021-01-07 17:09:30 -0600

    Initial setup

---
+++ src/containers/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../assets/styles/containers/App.scss';
+
+const App = () => {
+    return (
+        <div className="app">
+        </div>
+    );
+}
+
+export default App;
